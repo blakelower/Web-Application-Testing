c8d9890dcfbf3b2b8bcaa8c085637199333c932c
blakelower <blakelower1999@gmail.com>
2020-02-13 20:28:36 +0700

    added jsx to dashboard and display

---
+++ testing/src/components/dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <p>dashboard component</p>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ testing/src/components/display.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function display() {
+  return (
+    <div className="Display">
+      <h2>display component</h2>
+    </div>
+  );
+}
+
+export default display;
